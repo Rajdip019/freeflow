9e82d0ac2fcb56693342f05d3b5c533d2da5203d
Rajdeep Sengupta <rajdipgupta019@gmail.com>
2023-05-08 23:35:50 +0530

    Small fixes and random password generation done

--- components/ImageUploadModal.tsx
+++ components/ImageUploadModal.tsx
@@ -7,7 +7,6 @@ import {
   ModalBody,
   ModalCloseButton,
   ModalContent,
-  ModalFooter,
   ModalHeader,
   ModalOverlay,
   useDisclosure,
@@ -159,6 +158,12 @@ const ImageUploadModal = () => {
     }
   };
 
+  //generate random password
+  const generatePassword = () => {
+    const randomPassword = Math.random().toString(36).slice(-8);
+    setPassword(randomPassword);
+  };
+
   return (
     <>
       <button
@@ -257,6 +262,25 @@ const ImageUploadModal = () => {
                           className=" text-gray-200"
                           placeholder="Enter a password for image review"
                         />
+                        <button
+                          className=" mt-2 flex items-center text-sm text-purple-400 underline underline-offset-4"
+                          onClick={generatePassword}
+                        >
+                          Generate random password{" "}
+                          <svg
+                            fill="currentColor"
+                            className=" ml-2 w-4"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                            aria-hidden="true"
+                          >
+                            <path
+                              clipRule="evenodd"
+                              fillRule="evenodd"
+                              d="M4.755 10.059a7.5 7.5 0 0112.548-3.364l1.903 1.903h-3.183a.75.75 0 100 1.5h4.992a.75.75 0 00.75-.75V4.356a.75.75 0 00-1.5 0v3.18l-1.9-1.9A9 9 0 003.306 9.67a.75.75 0 101.45.388zm15.408 3.352a.75.75 0 00-.919.53 7.5 7.5 0 01-12.548 3.364l-1.902-1.903h3.183a.75.75 0 000-1.5H2.984a.75.75 0 00-.75.75v4.992a.75.75 0 001.5 0v-3.18l1.9 1.9a9 9 0 0015.059-4.035.75.75 0 00-.53-.918z"
+                            />
+                          </svg>
+                        </button>
                       </div>
                     </>
                   )}
