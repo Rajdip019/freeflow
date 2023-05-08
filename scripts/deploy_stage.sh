229e3dd20b73312f8c6eb7fd7a7e30c98389f937
Michael Young <michaeljy@gmail.com>
2023-05-07 17:46:26 -0700

    Make deploy messages easier to read

--- scripts/deploy_stage.sh
+++ scripts/deploy_stage.sh
@@ -1,8 +1,8 @@
 # Deploy to Vercel stage
 # Requires VERCEL_TOKEN to be set in .env.local
 eval $(grep '^VERCEL_TOKEN' ./.env.local)
-echo "Deploying to stage..."
+echo "🚀 Deploying to stage..."
 DEPLOYMENT_URL=$(vercel deploy --token=$VERCEL_TOKEN)
-echo "Deployment URL to alias to stage: $DEPLOYMENT_URL"
+echo "🚀 Setting alias to freeflow-stage: $DEPLOYMENT_URL"
 vercel alias set $DEPLOYMENT_URL freeflow-stage.vercel.app --token=$VERCEL_TOKEN
-./node_modules/.bin/vercel deploy --token $VERCEL_TOKEN --debug
+./node_modules/.bin/vercel deploy --token $VERCEL_TOKEN
