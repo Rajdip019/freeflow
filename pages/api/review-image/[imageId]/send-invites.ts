b85b137d47e536c5074edda1fd5b8bb0010fecad
Mukesh Kuiry <99157367+bbsdevloper@users.noreply.github.com>
2023-08-29 01:53:22 +0530

    Email testing

--- pages/api/review-image/[imageId]/send-invites.ts
+++ pages/api/review-image/[imageId]/send-invites.ts
@@ -4,7 +4,8 @@ import { db } from "@/lib/firebaseConfig";
 import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { uniq } from "lodash-es";
 import { NextApiRequest, NextApiResponse } from "next";
-import sgMail from "@sendgrid/mail";
+import { resend } from "@/lib/api/mailer";
+import DesignReviewInvite from "@/emails/DesignReviewInvite";
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -21,41 +22,30 @@ export default async function handler(
         return res.status(404).end();
       }
       const data = docSnap.data() as IReviewImageData;
-
-      let designReviewPassword: string = "";
-      if (data && data.isPrivate) {
-        const passwordDocRef = doc(
-          db,
-          "reviewImages",
-          imageId as string,
-          "private/password"
-        );
-        const passwordDocSnap = await getDoc(passwordDocRef);
-        if (passwordDocSnap) {
-          designReviewPassword = passwordDocSnap.data()?.password ?? "";
-        }
-      }
+      console.log("ImageId", imageId);
+      console.log("data", data);
 
       const designReviewUrl = `${APP_URL}/review-image/${imageId}`;
 
-      const { templateData } = req.body;
-      sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
       const sendPromises = emails.map(async (email: string) => {
-        const msg = {
-          to: email,
-          from: "unsnarl.secure@gmail.com",
-          templateId: "d-c1725864e64849639149782c58502a40",
-          dynamic_template_data: { templateData },
-        };
-        await sgMail.send(msg);
+        if (data)
+          await resend.sendEmail({
+            from: "Acme <onboarding@resend.dev>",
+            to: email,
+            subject: `[FreeFlow] ${data.uploadedBy} requested your feedback on a design`,
+            // @ts-expect-error
+            react: DesignReviewInvite({
+              designReviewUrl,
+              inviter: data.uploadedBy,
+            }),
+          });
       });
 
       await Promise.all(sendPromises);
 
       // Update sentEmailInvites field in db
-      const existingEmailInvites = data.sentEmailInvites
-        ? data.sentEmailInvites ?? []
-        : [];
+      const existingEmailInvites =
+        data && data.sentEmailInvites ? data.sentEmailInvites ?? [] : [];
       const updatedEmailInvites = uniq([...existingEmailInvites, ...emails]);
       await updateDoc(docRef, {
         sentEmailInvites: updatedEmailInvites,
